a41eb6bc04db73c6c39cecf66792a5b8ffa4258e
Laura Walters <laura.walters1098@yahoo.com>
2022-03-27 23:31:17 +0100

    adding login page and route

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("login");
 });
-router.use("/api", apiRoutes);
 
 module.exports = router;
