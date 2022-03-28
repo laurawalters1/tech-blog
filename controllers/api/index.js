66f41400ded0d60b7a6b216bd0a9d3233c0a490e
Laura Walters <laura.walters1098@yahoo.com>
2022-03-28 22:53:42 +0100

    Adding find user and check password functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-const loginRoutes = require("./login-routes");
+const userRoutes = require("./user-routes");
 
-router.use("/login", loginRoutes);
+router.use("/login-page", userRoutes);
 
 module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,45 @@
+const { User } = require("../../models");
+
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      console.log("no user data");
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      console.log("invalid password");
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    console.log("We found this user!");
+    res.status(200);
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;
+
+    //   res.json({ user: userData, message: "You are now logged in!" });
+    // });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
