28d3810e57fbdf2d696d16640a067cde2ef4961b
Laura Walters <laura.walters1098@yahoo.com>
2022-03-28 19:51:58 +0100

    altering home routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
+router.use("/", homeRoutes);
 
-router.get("/", (req, res) => {
-  res.render("homepage");
-});
 router.use("/api", apiRoutes);
 
 module.exports = router;
