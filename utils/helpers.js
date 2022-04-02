ac6b313fb852b80e0a25dca7223528b274a42e83
Laura Walters <laura.walters1098@yahoo.com>
2022-04-02 17:42:33 +0100

    adding date formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,8 @@ module.exports = {
   isEqual: (val1, val2) => {
     return val1 === val2;
   },
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
 };
